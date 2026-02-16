9f7dd2db230da1f7437542a7110cde68a949970a
Raad05 <yamin.raad6109@gmail.com>
2026-02-17 00:33:35 +0600

    vector exercise

--- topics/vector/exercises/src/lib.rs
+++ topics/vector/exercises/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn init(x: u32, y: u32, z: u32) -> Vec<u32> {
-    todo!();
+    // the easy way
+    // let v = vec![x, y, z];
+
+    // the hard way
+    let mut v: Vec<u32> = Vec::new();
+    v.push(x);
+    v.push(y);
+    v.push(z);
+
+    v
 }
