0a6e32d8ee6eecf5011e02b62e3593157c4f5d11
Raad05 <yamin.raad6109@gmail.com>
2026-03-10 01:45:17 +0600

    loop exercies

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,19 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut total: i32 = 0;
+
+    for n in nums {
+        total += n;
+    }
+
+    total
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut v: Vec<u32> = Vec::new();
+
+    while v.len() < n {
+        v.push(i);
+    }
+
+    v
 }
