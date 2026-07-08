19e805ba6667cad145e3b1d8218a51f242082cf0
Raad05 <yamin.raad6109@gmail.com>
2026-07-08 18:26:02 +0600

    iterator adaptors issue fix

--- topics/iterator_adaptors/exercises/src/lib.rs
+++ topics/iterator_adaptors/exercises/src/lib.rs
@@ -1,13 +1,16 @@
 use std::collections::HashMap;
 
 pub fn filter_non_zero(v: Vec<u32>) -> Vec<u32> {
-    todo!();
+    let result: Vec<u32> = v.into_iter().filter(|&val| val > 0).map(|x| x).collect();
+    result
 }
 
 pub fn to_string(v: Vec<&str>) -> Vec<String> {
-    todo!();
+    let result: Vec<String> = v.iter().map(|x| x.to_string()).collect();
+    result
 }
 
 pub fn to_hash_map(v: Vec<(String, u32)>) -> HashMap<String, u32> {
-    todo!();
+    let result: HashMap<String, u32> = v.iter().map(|x| (x.0.to_string(), x.1)).collect();
+    result
 }
